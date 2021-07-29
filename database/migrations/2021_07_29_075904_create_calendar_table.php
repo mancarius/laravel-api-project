f688c0b5560fa2e3bfac60384fc02ee5edfba29a
Mattia <75211269+mancarius@users.noreply.github.com>
2021-07-29 12:34:44 +0200

    creation

---
+++ database/migrations/2021_07_29_075904_create_calendar_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalendarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('day_id')->constraint()->onDelete('CASCADE');
+            $table->foreignId('waste_types_id')->constraint()->onDelete('CASCADE');
+            $table->time('time_start');
+            $table->time('time_interval')->default(3600);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar');
+    }
+}
