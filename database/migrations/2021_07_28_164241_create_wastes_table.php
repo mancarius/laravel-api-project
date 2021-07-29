de7639cdd52ee2aba5529075132aae4751f5103b
Mattia <75211269+mancarius@users.noreply.github.com>
2021-07-29 17:19:35 +0200

    added rows 'allowed' and 'not_allowed'

--- database/migrations/2021_07_28_164241_create_wastes_table.php
+++ database/migrations/2021_07_28_164241_create_wastes_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -17,6 +18,8 @@ class CreateWastesTable extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->text('description')->nullable();
+            $table->json('allowed')->default(new Expression('(JSON_ARRAY())'));
+            $table->json('not_allowed')->default(new Expression('(JSON_ARRAY())'));
             $table->string('color')->default('#ffffff');
             $table->timestamps();
         });
