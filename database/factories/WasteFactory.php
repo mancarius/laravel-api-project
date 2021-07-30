2bb632f30ee0d9335137afd299e93d4e56abeee5
Mattia <75211269+mancarius@users.noreply.github.com>
2021-07-30 16:56:06 +0200

    no actions

---
+++ database/factories/WasteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Waste;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WasteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Waste::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
