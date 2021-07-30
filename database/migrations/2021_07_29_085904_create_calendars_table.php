9504dafd5c691a5140fdc6ee0849fc863fb8140c
Mattia <75211269+mancarius@users.noreply.github.com>
2021-07-30 16:53:00 +0200

    fix: renamed calendar to calendars to fix seeding

--- database/migrations/2021_07_29_085904_create_calendars_table.php
+++ database/migrations/2021_07_29_085904_create_calendars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCalendarTable extends Migration
+class CreateCalendarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCalendarTable extends Migration
      */
     public function up()
     {
-        Schema::create('calendar', function (Blueprint $table) {
+        Schema::create('calendars', function (Blueprint $table) {
             $table->id();
             $table->foreignId('day_id')
                 ->constrained('days')
@@ -37,6 +37,6 @@ class CreateCalendarTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('calendar');
+        Schema::dropIfExists('calendars');
     }
 }
