f8e4d526b2b0299e9caa82f9f68da63e58bbc262
Mattia <75211269+mancarius@users.noreply.github.com>
2021-08-02 18:53:07 +0200

    added days function

--- app/Models/Waste.php
+++ app/Models/Waste.php
@@ -10,4 +10,10 @@ class Waste extends Model
     use HasFactory;
 
     public $timestamps = true;
+    protected $hidden = ['pivot'];
+
+    public function days () {
+        return $this->belongsToMany(Day::class, 'waste_days')
+                    ->select(['days.*', 'waste_days.collection_time_start','waste_days.collection_time_interval']);
+    }
 }
