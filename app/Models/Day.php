ec8924b59f80ba3d27d4147384586682ed6c88be
Mattia <75211269+mancarius@users.noreply.github.com>
2021-08-02 18:52:48 +0200

    added wastes function

--- app/Models/Day.php
+++ app/Models/Day.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Day extends Model
 {
     use HasFactory;
+
+    protected $hidden = ['pivot'];
+
+    public function wastes () {
+        return $this->belongsToMany(Waste::class, 'waste_days')
+                    ->select(['wastes.*', 'waste_days.collection_time_start', 'waste_days.collection_time_interval']);
+    }
 }
