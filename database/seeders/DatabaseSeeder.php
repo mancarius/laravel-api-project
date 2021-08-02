3b51c64e6d9c862fe25578754782fee3e6d2fb88
Mattia <75211269+mancarius@users.noreply.github.com>
2021-08-02 18:53:53 +0200

    fix: renamed CalendarSeeder to WasteDaySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             WasteSeeder::class,
             DaySeeder::class,
-            CalendarSeeder::class,
+            WasteDaySeeder::class,
         ]);
     }
 }
