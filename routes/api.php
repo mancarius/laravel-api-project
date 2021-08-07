547aab1aec63dba6c6a55ae051852d10dfe714f0
Mattia <75211269+mancarius@users.noreply.github.com>
2021-08-07 10:14:06 +0200

    implemented Route::resource instead of singles rest method

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\DayController;
-use App\Http\Controllers\WasteController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/day/{id?}', [DayController::class, 'show'])->name('day');
+Route::resource('waste', App\Http\Controllers\WasteController::class)->only(['index', 'show']);
 
+Route::resource('day', App\Http\Controllers\DayController::class)->only(['index', 'show']);
 
-Route::get('/waste/{id?}', [WasteController::class, 'show'])->name('waste');
-
-
-Route::post('/day/{day}/{waste}', function (Request $request) {
-
-});
+Route::resource('collection', App\Http\Controllers\WasteDayController::class)->except(['index', 'show', 'create', 'edit']);
