5c1fc5f40e4fcda2162028ce16fe8d950079ae48
Mattia <75211269+mancarius@users.noreply.github.com>
2021-08-08 18:16:49 +0200

    added optional param $pick_up_to_exclude to isSlotAvailableInDay() for excluding a pick up from the search

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -52,7 +52,7 @@ class DayController extends Controller
     }
     
     /**
-     * Returns true if the given waste collection is scheduled in the given day.
+     * Returns true if the given waste pick-up is scheduled in the given day.
      *
      * @param int $waste_id
      * @param int $day_id
@@ -73,13 +73,14 @@ class DayController extends Controller
      * @param int $day_id
      * @param string $time_start
      * @param string $time_end
+     * @param string $pick_up_to_exclude The pick up key to exclude from the calculation (optional)
      * @return boolean
      */
-    static function isSlotAvailableInDay($day_id, $time_start, $time_end): bool
+    static function isSlotAvailableInDay($day_id, $time_start, $time_end, $pick_up_to_exclude = ''): bool
     {
         $new_time_start = new \DateTime($time_start);
         $new_time_end   = new \DateTime($time_end);
-        $wastes_collection = Day::findOrFail($day_id)->wastes;
+        $wastes_pick_up = Day::findOrFail($day_id)->wastes;
 
         function isTimeInInterval($time_test, $time_start, $time_end) {
             if( $time_start < $time_test && $time_end > $time_test )
@@ -88,9 +89,11 @@ class DayController extends Controller
                 return false;
         }
 
-        foreach ($wastes_collection as $waste) {
-            $test_time_start = new \DateTime( $waste->collection_time_start );
-            $test_time_end   = new \DateTime( $waste->collection_time_end );
+        foreach ($wastes_pick_up as $waste) {
+            if ($waste->pick_up_id === $pick_up_to_exclude)
+                continue;
+            $test_time_start = new \DateTime( $waste->pick_up_time_start );
+            $test_time_end   = new \DateTime( $waste->pick_up_time_end );
             
             if ( isTimeInInterval($new_time_start, $test_time_start, $test_time_end) )
                 return false;
